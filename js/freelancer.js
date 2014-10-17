03ee5128770d90fd709d58696adbb8ad52826a08
Eduardo Robles Elvira <edulix@wadobo.com>
2014-10-17 09:22:52 +0200

    fixing cookies notification

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,36 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+$(document).ready(function () {
+  var cookie = readCookie("do-not-show-cookies-header");
+  if (cookie !== "true") {
+    $("#cookies-alert").css("display", "block");
+  }
+});
+
+$("#closecookies").click(function (e) {
+  e.preventDefault();
+  createCookie("do-not-show-cookies-header", "true", 3600);
+});
